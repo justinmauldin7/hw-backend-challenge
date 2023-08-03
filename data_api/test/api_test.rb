326fbf8c546434c3ea96ac3a01641656d517aaa0
Justin Mauldin <justin.mauldin7@gmail.com>
2023-08-03 17:30:51 -0600

    Adds test file for the API endpoints in the data_api repo.

---
+++ data_api/test/api_test.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+
+require_relative '../api'
+
+describe API do
+    describe "Summary Response" do
+       it "Gets Summary" do
+            skip
+       end
+    end
+
+    describe "Client Upload Response" do
+       it "Posts Client Uploads" do
+            skip
+       end
+    end
+end
